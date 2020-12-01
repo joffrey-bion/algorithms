039f13774882418ec6f4e7a512ff817cb331c1e3
Joffrey Bion <joffrey.bion@gmail.com>
2020-12-02 00:27:36 +0100

    Add permutations function

--- src/main/kotlin/org/hildan/algorithms/strings/InputAndStrings.kt
+++ src/main/kotlin/org/hildan/algorithms/strings/InputAndStrings.kt
@@ -8,3 +8,19 @@ fun i()=readLine()!!.split(" ").map{it.toInt()}
 fun String.digits(): List<Int> = map { "$it".toInt() }
 
 fun String.asciiChars(): List<Int> = map { it.toInt() }
+
+fun permutations(s: String): Set<String> {
+    var perms = mutableSetOf<String>("")
+    for (c in s) {
+        perms = perms.flatMapTo(HashSet()) { it.insertEverywhere(c) }
+    }
+    return perms
+}
+
+private fun String.insertEverywhere(c: Char): Set<String> {
+    val result = mutableSetOf<String>()
+    for (i in 0..length) {
+        result.add(substring(0, i) + c + substring(i))
+    }
+    return result
+}
