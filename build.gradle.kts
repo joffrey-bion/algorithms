e5151f2a9187c72b53e810660faf8a4dd1c0898a
Joffrey Bion <joffrey.bion@gmail.com>
2022-02-17 01:55:58 +0100

    Upgrade kotlin and deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,25 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.72" // used by codingame
+    kotlin("jvm") version "1.6.10" // used by codingame
 }
 
 description = "Search and sort algorithms for practice and educational purpose."
 
 repositories {
     mavenCentral()
-    jcenter()
 }
 
 dependencies {
-    implementation("org.jetbrains:annotations:15.0")
-    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains:annotations:22.0.0")
+
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit5"))
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "17"
 }
 
 tasks {
